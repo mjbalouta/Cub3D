dadc3b6e8c00b5ce9b5fc42215d20396d2f61a7a
Bruna Maria Duarte Barbosa <brmaria-@c1r11s2.42porto.com>
2026-01-15 12:39:10 +0000

    some changes

--- srcs/raycasting/draw_walls.c
+++ srcs/raycasting/draw_walls.c
@@ -1,11 +1,11 @@
 #include "cub3d.h"
 
-void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
-{
-	char	*dst;
+// void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+// {
+// 	char	*dst;
 
-	if (x < 0 || y < 0)
-		return ;
-	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	*(unsigned int *)dst = color;
-}
+// 	if (x < 0 || y < 0)
+// 		return ;
+// 	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+// 	*(unsigned int *)dst = color;
+// }
--- srcs/raycasting/start_ray.c
+++ srcs/raycasting/start_ray.c
@@ -1,5 +1,15 @@
 #include "cub3d.h"
 
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x < 0 || y < 0)
+		return ;
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
 int	get_texture_index(t_ray *ray)
 {
 	if (ray->side == 0 && ray->ray_dir_x > 0)
