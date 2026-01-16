bb40395a28876ae502c16a54c7525281671eb3db
mjbalouta <mjbalouta@gmail.com>
2026-01-16 14:42:23 +0000

    fix: removed ' ' validation in move

--- srcs/game/moves.c
+++ srcs/game/moves.c
@@ -5,25 +5,21 @@ void	move_up(t_game *game, double new_y, double new_x)
 	int	next_y;
 	int	next_x;
 
-	// Check Y movement
 	next_y = (int)(game->player.posy + (new_y * BOUNDARY));
 	next_x = (int)(game->player.posx);
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posy += new_y;
 	}
-	// Check X movement
 	next_y = (int)(game->player.posy);
 	next_x = (int)(game->player.posx + (new_x * BOUNDARY));
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posx += new_x;
 	}
@@ -34,25 +30,21 @@ void	move_down(t_game *game, double new_y, double new_x)
 	int	next_y;
 	int	next_x;
 
-	// Check Y movement
 	next_y = (int)(game->player.posy - (new_y * BOUNDARY));
 	next_x = (int)(game->player.posx);
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posy -= new_y;
 	}
-	// Check X movement
 	next_y = (int)(game->player.posy);
 	next_x = (int)(game->player.posx - (new_x * BOUNDARY));
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posx -= new_x;
 	}
@@ -63,25 +55,21 @@ void	move_right(t_game *game, double new_y, double new_x)
 	int	next_y;
 	int	next_x;
 
-	// Check Y movement
 	next_y = (int)(game->player.posy + (new_y * BOUNDARY));
 	next_x = (int)(game->player.posx);
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posy += new_y;
 	}
-	// Check X movement
 	next_y = (int)(game->player.posy);
 	next_x = (int)(game->player.posx + (new_x * BOUNDARY));
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posx += new_x;
 	}
@@ -92,25 +80,21 @@ void	move_left(t_game *game, double new_y, double new_x)
 	int	next_y;
 	int	next_x;
 
-	// Check Y movement
 	next_y = (int)(game->player.posy - (new_y * BOUNDARY));
 	next_x = (int)(game->player.posx);
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posy -= new_y;
 	}
-	// Check X movement
 	next_y = (int)(game->player.posy);
 	next_x = (int)(game->player.posx - (new_x * BOUNDARY));
 	if (next_x >= 0 && next_y >= 0
 		&& next_y < game->map.height && game->map.map[next_y]
 		&& next_x < (int)ft_strlen(game->map.map[next_y])
-		&& game->map.map[next_y][next_x] != '1'
-		&& game->map.map[next_y][next_x] != ' ')
+		&& game->map.map[next_y][next_x] != '1')
 	{
 		game->player.posx -= new_x;
 	}
