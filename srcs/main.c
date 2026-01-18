d4be462191724288b278077d39f315e3d1002f7a
mjoao-fr <mjbalouta@gmail.com>
2026-01-18 20:46:26 +0000

    fix: fixed position of player when turned to Weast

--- srcs/main.c
+++ srcs/main.c
@@ -1,45 +1,5 @@
 #include "../includes/cub3d.h"
 
-// void	print_parsing(t_game *game)
-// {
-// 	int i = 0;
-
-// 	ft_printf("\nTEXTURES:\n");
-// 	while (i < 4)
-// 	{
-// 		ft_printf("%d  %s\n", game->texture[i].defined, game->texture[i].path);
-// 		i++;
-// 	}
-// 	ft_printf("\nCOLORING:\n");
-// 	i = 0;
-// 	ft_printf("F %d ", game->floor_color.defined);
-// 	while (i < 3)
-// 	{
-// 		ft_printf("%d", game->floor_color.rgb[i]);
-// 		if (i != 2)
-// 			ft_printf(",");
-// 		i++;
-// 	}
-// 	i = 0;
-// 	ft_printf("\nS %d ", game->sky_color.defined);
-// 	while (i < 3)
-// 	{
-// 		ft_printf("%d", game->sky_color.rgb[i]);
-// 		if (i != 2)
-// 			ft_printf(",");
-// 		i++;
-// 	}
-// 	ft_printf("\n");
-// 	ft_printf("\nMAP:\n");
-// 	i = 0;
-// 	while (i < game->map.height)
-// 	{
-// 		ft_printf("%s", game->map.map[i]);
-// 		i++;
-// 	}
-// 	ft_printf("\n");
-// }
-
 int	main(int ac, char **av)
 {
 	t_game	game;
